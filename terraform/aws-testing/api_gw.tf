7f4bdbbffbaebf06235513592638c56f6ff28c7a
Vijay Ap <appaniv@vmware.com>
2023-07-07 12:48:47 -0400

    APIGW , Lambda, RDS

--- terraform/aws-testing/api_gw.tf
+++ terraform/aws-testing/api_gw.tf
@@ -3,7 +3,6 @@ resource "aws_api_gateway_rest_api" "example" {
   description = "Terraform Serverless Application Example"
 }
 
-
 output "base_url" {
   value = "${aws_api_gateway_deployment.example.invoke_url}"
 }
